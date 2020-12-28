892cb19cd66cc3b594de9d8a3d76a5c4d2c4860b
SOUFIANEZAZA <72149789+SOUFIANEZAZA@users.noreply.github.com>
2020-12-28 05:11:01 +0100

    Update MainBot.py

--- BotData/MainBot.py
+++ BotData/MainBot.py
@@ -88,12 +88,12 @@ def ig_masslooker():
                     bot.logger.info("Total stories viewed: %d" % bot.total["stories_viewed"])
 
                 # CHOOSE RANDOM LIKER TO GRAB HIS LIKERS AND REPEAT
-                countdown(180000)
+                countdown(180)
             except Exception as e:
                 # If something went wrong - sleep long and start again
                 bot.logger.info(e)
                 current_user_id = user_to_get_likers_of
-                time.sleep(180000 * random.random() + 180000)
+                time.sleep(0 * random.random() + 0)
 
         except KeyboardInterrupt:
             break
